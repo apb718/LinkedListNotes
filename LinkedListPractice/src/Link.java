56ada1bef00eabbd7aec8c5298fe846e0450f296
Apb718 <apb718@gmail.com>
2022-08-28 14:52:55 -0400

    Added Link and LinkedList classes

--- LinkedListPractice/src/Link.java
+++ LinkedListPractice/src/Link.java
@@ -1,4 +1,22 @@
 
 public class Link {
 
+	double dData;
+	int iData;
+	Link next;
+	
+	public Link() {
+		iData = 0;
+		dData = 0.0;
+	}
+	
+	public Link(int iData, double dData) {
+		this.iData = iData;
+		this.dData = dData;
+	}
+	
+	public void displayLink() {
+		System.out.println("{"+iData+", " + dData+"} ");
+	}
+	
 }
--- LinkedListPractice/src/LinkedList.java
+++ LinkedListPractice/src/LinkedList.java
@@ -1,4 +1,37 @@
 
 public class LinkedList {
-
+	private Link first;
+	
+	public LinkedList(Link first) {
+		this.first = first;
+	}
+	public LinkedList() {
+		first = null;
+	}
+	
+	public void insertFirst(int iData, double dData) {
+		Link newLink = new Link(iData, dData);
+		
+		newLink.next = first;
+		
+		first = newLink;
+	}
+	
+	public void deleteFirst() {
+		first = first.next;
+	}
+	
+	public boolean isEmpty() {
+		if(first.next == null) {
+			return true;
+		}
+		return false;
+	}
+	public void displayList() {
+		Link current = first;
+		while(current != null) {
+			current.displayLink();
+			current = current.next;
+		}
+	}
 }
